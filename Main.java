a0d04a99731f35755fc2ec385a478b71a3a4c9ed
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-29 12:56:28 +0000

    calcula a média e as maiores e menores notas

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
 public static void main(String[] args) {
   String nome = "";
-  float num1 = 0, num2 = 0, num3, num4 = 0;
-  double total = 0, media = 0;
+  float num1 = 0, num2 = 0, num3, num4 = 0, num5 = 0, notas;
+  double total = 0, media = 0, maior = 0, menor = 10;
 Scanner sc1 = new Scanner(System.in);
 
 System.out.println("Vamos calcular sua média em matemática");
@@ -12,20 +12,19 @@ System.out.println("");
 System.out.print("Digite o seu nome: ");
 nome = sc1.nextLine();
 
-System.out.print("Informe a 1° nota: ");
-num1 = sc1.nextFloat();
-
-System.out.print("Informe a 2° nota: ");
-num2 = sc1.nextFloat();
-
-System.out.print("Informe a 3° nota: ");
-num3 = sc1.nextFloat();
-
-System.out.print("Informe a 4° nota: ");
-num4 = sc1.nextFloat();
-
-total = num1 + num2  +num3 + num4;
-media = total/4;
-System.out.println(nome + " a sua media é: " + media);
+  for (int i=0; i<5; i++) {
+    System.out.print("Digite suas cinco notas: ");
+      notas = sc1.nextFloat();
+     total += notas;
+     if (notas > maior) {
+      maior = notas;
+    } else if (notas < menor){
+      menor = notas;
+  }
+}
+  media = total/5;
+  
+  System.out.println (nome + " A sua média é: " + media);
+  System.out.println(nome + " A sua maior nota é: " + maior + " e sua menor nota é: " + menor);
 }
 }
